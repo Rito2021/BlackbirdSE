619996eaae0d6e0dc2a53dc40ea248b55e89fe46
Xinyu <93416591+Rito2021@users.noreply.github.com>
2023-08-16 23:52:24 -0400

    Update command.sh

--- my-app/command.sh
+++ my-app/command.sh
@@ -4,4 +4,12 @@ npm start
 npm test
 npm run build
 
+git add .
+git commit -m "BlackbirdSE"
+git push origin update_logo
+
+gh pr create
+
+gh pr merge
+
 # REPO_URL <https://github.com/Rito2021/BlackbirdSE.git>
